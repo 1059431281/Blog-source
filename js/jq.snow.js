167d5bb7f0d29cc23e58590f87b9be9aa70e37fc
1059431281 <1059431281@qq.com>
2020-05-07 21:21:52 +0800

    w

---
+++ js/jq.snow.js
@@ -0,0 +1,54 @@
+(function($){
+    var interval
+    $.fn.snow = function(options){
+
+        var $flake 			= $('<div id="snowbox" />').css({'position': 'fixed', 'top': '-50px','z-index':'9999'}).html('&#10052;'),
+            documentHeight 	= $(document).height(),
+            documentWidth	= $(document).width(),
+            defaults		= {
+                minSize		: 10,		//雪花的最小尺寸
+                maxSize		: 20,		//雪花的最大尺寸
+                newOn		: 1000,		//雪花出现的频率
+                flakeColor	: "#FFFFFF"
+            },
+            options			= $.extend({}, defaults, options);
+
+         interval		= setInterval( function(){
+            var startPositionLeft 	= Math.random() * documentWidth - 100,
+                startOpacity		= 0.5 + Math.random(),
+                sizeFlake			= options.minSize + Math.random() * options.maxSize,
+                // endPositionTop		= documentHeight - 40,
+                endPositionTop = $('#xuehua').height() - 100
+                // endPositionLeft		= startPositionLeft - 100 + Math.random() * 500,
+                endPositionLeft = 0
+                durationFall		= documentHeight * 10 + Math.random() * 5000;
+            $flake.clone().appendTo($('.app')).css({
+                left: startPositionLeft,
+                opacity: startOpacity,
+                'font-size': sizeFlake,
+                color: options.flakeColor
+            }).animate({
+                    top: endPositionTop,
+                    opacity: 0.2
+                },durationFall,'linear',function(){
+                    $(this).remove()
+                }
+            );
+
+        }, options.newOn);
+
+    };
+
+    // window.addEventListener("resize",function () {
+    //     clearInterval(interval);
+    //
+    //     $(function(){
+    //         $.fn.snow({
+    //             minSize: 5,		//雪花的最小尺寸
+    //             maxSize: 40, 	//雪花的最大尺寸
+    //             newOn: 100		//雪花出现的频率 这个数值越小雪花越多
+    //         });
+    //     });
+    // },false)
+
+})(jQuery);
